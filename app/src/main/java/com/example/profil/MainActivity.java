4459f11f8a13d3dd3017db5f86a698735a9600d9
muhammadalihasymi <56553081+muhammadalihasymi@users.noreply.github.com>
2019-12-28 14:50:03 +0700

    Add files via upload

--- app/src/main/java/com/example/profil/MainActivity.java
+++ app/src/main/java/com/example/profil/MainActivity.java
@@ -1,13 +1,94 @@
 package com.example.profil;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.res.Resources;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+import com.example.profil.Helper.LocaleHelper;
+
+import io.paperdb.Paper;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView textView;
+    TextView textView2;
+    TextView textView3;
+    TextView textView4;
+    TextView textView5;
+    TextView textView6;
+    TextView textView7;
+    TextView textView8;
+    TextView textView9;
+    TextView textView10;
+
+    @Override
+    protected void attachBaseContext(Context newBase) {
+        super.attachBaseContext(LocaleHelper.onAttach(newBase, "en"));
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textView = (TextView)findViewById(R.id.textView);
+        textView2 = (TextView)findViewById(R.id.textView2);
+        textView3 = (TextView)findViewById(R.id.textView3);
+        textView4 = (TextView)findViewById(R.id.textView4);
+        textView5 = (TextView)findViewById(R.id.textView5);
+        textView6 = (TextView)findViewById(R.id.textView6);
+        textView7 = (TextView)findViewById(R.id.textView7);
+        textView8 = (TextView)findViewById(R.id.textView8);
+        textView9 = (TextView)findViewById(R.id.textView9);
+        textView10 = (TextView)findViewById(R.id.textView10);
+
+        Paper.init(this);
+
+        String language = Paper.book().read("Language");
+        if(language == null)
+            Paper.book().write("language","en");
+        
+        updateView((String)Paper.book().read("language"));
+    }
+
+    private void updateView(String lang) {
+        Context context = LocaleHelper.setLocale(this, lang);
+        Resources resources = context.getResources();
+
+        textView.setText(resources.getString(R.string.MalangCity));
+        textView2.setText(resources.getString(R.string.Follower));
+        textView3.setText(resources.getString(R.string.Posting));
+        textView4.setText(resources.getString(R.string.Following));
+        textView5.setText(resources.getString(R.string.Email));
+        textView6.setText(resources.getString(R.string.Number));
+        textView7.setText(resources.getString(R.string.Project));
+        textView8.setText(resources.getString(R.string.Project1));
+        textView9.setText(resources.getString(R.string.Hobby));
+        textView10.setText(resources.getString(R.string.Hobby1));
+     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if(item.getItemId() == R.id.language_en){
+            Paper.book().write("language","en");
+            updateView((String)Paper.book().read("language"));
+        }
+        else if(item.getItemId() == R.id.language_in){
+            Paper.book().write("language","in");
+            updateView((String)Paper.book().read("language"));
+        }
+        return true;
     }
 }
---
+++ app/src/main/java/com/example/profil/MainApplication.java
@@ -0,0 +1,13 @@
+package com.example.profil;
+
+import android.app.Application;
+import android.content.Context;
+
+import com.example.profil.Helper.LocaleHelper;
+
+public class MainApplication extends Application {
+    @Override
+    protected void attachBaseContext(Context base) {
+        super.attachBaseContext(LocaleHelper.onAttach(base,"en"));
+    }
+}
